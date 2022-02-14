85e25f812e720201d21d0d67c3e29bd8810ca31e
Robeer, M.J. (Marcel) <m.j.robeer@uu.nl>
2022-02-14 21:03:19 +0000

    Renamed models to model

--- explabox/__init__.py
+++ explabox/__init__.py
@@ -3,7 +3,7 @@
 from explabox._version import __version__
 from explabox.data import import_data, train_test_split
 from explabox.explainability import Explainer
-from explabox.models import from_sklearn
+from explabox.model import from_sklearn
 from explabox.sensitivity import (OneToManyPerturbation, OneToOnePerturbation,
                                   compare_accuracy, compare_metric,
                                   compare_precision, compare_recall,
