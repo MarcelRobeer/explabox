441734639a0b88d20496ca2a2f698488a8d737d4
Marcel Robeer <marcel_robeer@hotmail.com>
2022-02-28 12:02:18 +0100

    Imports in explabox init

--- explabox/__init__.py
+++ explabox/__init__.py
@@ -12,10 +12,23 @@ from explabox.mixins import IngestiblesMixin
 
 
 class Explabox(Readable, IngestiblesMixin):
-    def __init__(self):
-        self.data = None
-        self.model = None
-        self.labelprovider = None
+    def __init__(self, data, model, labelprovider, train=None, test=None, ingestibles=None):
+        if ingestibles is not None:
+            self.from_ingestibles(ingestibles, required=['data', 'model', 'labelprovider'])
+        else:
+            if self.data is None and train is not None or test is not None:
+                self.data = {}
+                if train is not None:
+                    self.data['train'] = train
+                if test is not None:
+                    self.data['test'] = test
+            elif self.data is not None:
+                self.data = data
+            else:
+                raise ValueError('data, train or test should be provider for using explabox')
+            self.model = model
+            self.labelprovider = labelprovider
+
         self.examine = Examiner(ingestibles=self.ingestibles)
         self.explain = Explainer(ingestibles=self.ingestibles)
         self.explore = Explorer(ingestibles=self.ingestibles)
