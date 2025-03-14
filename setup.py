b0e2ab3a2bc5f4f0d1f62757360ed8199d4135b7
Marcel Robeer <marcel_robeer@hotmail.com>
2025-03-14 12:26:00 +0100

    Requirements update
    Signed-off-by: Marcel Robeer <marcel_robeer@hotmail.com>

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ extras["all"] = list(set([i for subi in extras.values() for i in subi]))
 
 setuptools.setup(  # type: ignore
     name="explabox",
-    version="1.0.0",
+    version="1.0.1",
     description="Explore/examine/explain/expose your model with the explabox!",
     long_description=long_description,
     long_description_content_type="text/markdown",
